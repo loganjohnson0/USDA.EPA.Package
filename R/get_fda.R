60ef5c3086077c8e9bf00ce74c071b4aaa84436f
Logan Johnson <logan01@iastate.edu>
2023-04-12 09:36:17 -0500

    this update interacts with the FDA api

--- R/get_fda.R
+++ R/get_fda.R
@@ -2,57 +2,75 @@
 #'
 #' @param x Need to update the input functionality of the function
 #'
-#' @importFrom dplyr bind_rows
-#' @importFrom dplyr %>%
-#' @importFrom netstat free_port
-#' @importFrom readr write_csv
-#' @importFrom RSelenium remoteDriver
-#' @importFrom rvest html_nodes
-#' @importFrom rvest html_table
-#' @importFrom xml2 read_html
-get_fda <- function(x) {
+#' @importFrom httr GET
+#' @importFrom jsonlite fromJSON
+#' @importFrom tibble tibble
+get_fda <- function(api_key,
+                    address_1 = NULL,
+                    address_2 = NULL,
+                    center_classification_date = NULL,
+                    city = NULL,
+                    classification = NULL,
+                    code_info = NULL,
+                    country = NULL,
+                    distribution_pattern = NULL,
+                    event_id = NULL,
+                    initial_firm_notifcation = NULL,
+                    more_code_info = NULL,
+                    product_code = NULL,
+                    product_description = NULL,
+                    product_quantity = NULL,
+                    product_type = NULL,
+                    reason_for_recall = NULL,
+                    recall_inititaion_date = NULL,
+                    recall_number = NULL,
+                    recalling_firm = NULL,
+                    report_date = NULL,
+                    state = NULL,
+                    status = Onoing,
+                    termination_date = NULL,
+                    voluntary_mandated = NULL) {
 
-  rD <- RSelenium::rsDriver(browser = "chrome",
-                            chromever = "112.0.5615.49",
-                            verbose = FALSE,
-                            port = netstat::free_port(random = TRUE),
-                            iedrver = NULL)
-
-  remDr <- rD[["client"]]
-
-  remDr$navigate("https://www.fda.gov/safety/recalls-market-withdrawals-safety-alerts")
-
-  remDr$screenshot(display = TRUE)
-
-  doc <- xml2::read_html(remDr$getPageSource()[[1]])
-
-  first_table_fda <- doc %>%
-    rvest::html_nodes("#datatable") %>%
-    rvest::html_table()
-
-  combined <- first_table_fda[[1]]
-
-  last_page <- doc %>%
-    rvest::html_node("#datatable_ellipsis+ .paginate_button a") %>%
-    rvest::html_text() %>%
-    as.numeric()
-
-  for (i in 1:2) { # This doesn't work with the "last page element"
-    if (i > 1) {
-      element <- remDr$findElement(using = 'css selector', "#datatable_next a")
-      element$clickElement()
-      Sys.sleep(1)
-      remDr$screenshot(display = TRUE)
-      doc <- xml2::read_html(remDr$getPageSource()[[1]])
-
-      fda_table <- doc %>%
-        rvest::html_nodes("#datatable") %>%
-        rvest::html_table()
-      fda_table <- fda_table[[1]]
-      combined <- dplyr::bind_rows(combined, fda_table)
-    }
+  if (!is.null(api_key)) {
+    api_key <- paste0(api_key, "&")
   }
-  # readr::write_csv(combined, file = "2023_04_11_FDA_Data.csv")
-  return(combined)
-  remDr$close()
+
+
+  core_url <- "https://api.fda.gov/food/enforcement.json?"
+  search <- "search="
+  key <- "api_key="
+  api_key <- api_key
+  status <- "status:Ongoing"
+  limit <- "&limit=1000"
+
+  fda_data <- httr::GET(url = paste0(core_url, key, api_key, search, status, limit))
+
+  data <- jsonlite::fromJSON(content(fda_data, "text"))
+
+  new_stuff <- tibble::tibble(recall_number = data$results$recall_number,
+                  recalling_firm = data$results$recalling_firm,
+                  recall_inititaion_date = data$results$recall_inititaion_date,
+                  center_classification_date = data$results$center_classification_date,
+                  report_date = data$results$report_date,
+                  voluntary_mandated = data$results$voluntary_mandated,
+                  classification = data$results$classification,
+                  initial_firm_notifcation = data$results$initial_firm_notification,
+                  status = data$results$status,
+                  country = data$results$country,
+                  state = data$results$state,
+                  city = data$results$city,
+                  address_1 = data$results$address_1,
+                  address_2 = data$results$address_2,
+                  postal_code = data$results$postal_code,
+                  reason_for_recall = data$results$reason_for_recall,
+                  product_description = data$results$product_description,
+                  product_quantity = data$results$product_quantity,
+                  code_info = data$results$code_info,
+                  more_code_info = data$results$more_code_info,
+                  distribution_pattern = data$results$distribution_pattern,
+                  event_id = data$results$event_id,
+                  product_type = data$results$product_type)
+  return(new_stuff)
+
 }
+
