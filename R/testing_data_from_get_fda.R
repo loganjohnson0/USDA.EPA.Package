43c3da87053be33814ca4282120b8eca02610336
Nascimento Thompson <kellynay@iastate.edu>
2023-04-14 18:06:14 -0500

    fda food recall data frame formatting

--- R/testing_data_from_get_fda.R
+++ R/testing_data_from_get_fda.R
@@ -22,3 +22,11 @@ df$event_id <- as.numeric(df$event_id)
 df$report_date <- ymd(df$report_date)
 df$recall_initiation_date <- ymd(df$recall_initiation_date)
 df$center_classification_date <- ymd(df$center_classification_date)
+
+# arranging the rows from "report_date" column in chronological order
+df <- arrange(df,desc(report_date))
+
+# add year of "report_date" as a column named "report_year", so later we can visualize
+# classification types (Class I, Class II, Class III) of recall by year and state, for instance
+# add day of the week as a column
+df <- df %>% mutate(report_year = year(report_date))
