b4cadefe3222a45145e26f3e39072f9c880541ba
Logan Johnson <logan01@iastate.edu>
2023-05-09 14:04:14 -0500

    shiny runExample function

---
+++ R/runExample.R
@@ -0,0 +1,17 @@
+#' Function to run the shiny app
+#'
+#' @param x shiny app to run
+#'
+#' @importFrom shiny runApp
+#'
+#' @export
+runExample <- function(x) {
+  appDir <- system.file("shiny-examples", "myapp",
+                        package = "foodRecall")
+  if (appDir == "") {
+    stop(paste0("Could not find example directory. ",
+                "Try re-installing `foodRecall`."), call. = FALSE)
+  }
+  # the first app will be called
+  shiny::runApp(appDir = appDir)
+}
