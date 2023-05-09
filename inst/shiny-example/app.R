01bb54f68de07941356d3fcd7607e8d334f308ab
Logan Johnson <logan01@iastate.edu>
2023-05-09 15:33:49 -0500

    This works for me.

--- inst/shiny-example/app.R
+++ inst/shiny-example/app.R
@@ -2,16 +2,17 @@ library(leaflet)
 library(shiny)
 library(DT)
 library(tidyverse)
+library(foodRecall)
 
 #' @importFrom leaflet leafletProxy, clearMarkers, addMarkers
-
+#' @importFrom lubridate date, mdy_hm
+#'
 # ui object
 ui <- fluidPage(
   titlePanel(p("FoodRecall", style = "color:#3474A7")),
   sidebarLayout(
     sidebarPanel(
       textInput("api_key", "Enter your API key from FDI:"),
-      numericInput("limit", "Limit", value = 1000),
       textInput("city", "City"),
       #textInput("country", "Country"),
       #textInput("distribution_pattern", "Distribution Pattern"),
@@ -29,7 +30,7 @@ ui <- fluidPage(
 
     ),
     mainPanel(
-      tableOutput("recall_table")
+      tableOutput(outputId = "recall_table")
     )
   )
 )
@@ -38,12 +39,9 @@ ui <- fluidPage(
 
 
 
-#' @importFrom lubridate date, mdy_hm
-
-
 server <- function(input, output) {
 
-  api_key <- "kgeTDxh6dhWwVpfa7fEzfbxFuqbeMpWe6gobqpcM"
+
 
   # Create reactive values for storing recall data and search results
   recall_data <- reactiveValues()
@@ -52,39 +50,34 @@ server <- function(input, output) {
   # Function to update search results when submit button is pressed
   observeEvent(input$submit_button, {
     # Call the recall_location() function and store results in recall_data
-    recall_data$recall_df <- recall_location(api_key = api_key,
-                                             limit = input$limit,
-                                             city = input$city,
-                                             #country = input$country,
-                                             #distribution_pattern = input$distribution_pattern,
-                                             recalling_firm = input$recalling_firm,
-                                             #search_mode = input$search_mode,
-                                             state = input$state,
-                                             status = input$status)
+    recall_data$recall_df <- foodRecall::recall_location(api_key = api_key,
+                                                         city = input$city,
+                                                         #country = input$country,
+                                                         #distribution_pattern = input$distribution_pattern,
+                                                         recalling_firm = input$recalling_firm,
+                                                         #search_mode = input$search_mode,
+                                                         state = input$state,
+                                                         status = input$status)
 
     # Filter the recall data based on user inputs and store in search_results
-    search_results$search_df <- recall_data$recall_df %>%
-      filter(recalling_firm %in% input$recalling_firm) %>%
-      filter(city %in% input$city) %>%
-      filter(state %in% input$state) %>%
-      #filter(country %in% input$country) %>%
-      #filter(distribution_pattern %in% input$distribution_pattern) %>%
-      filter(status %in% input$status)
+    # search_results$search_df <- recall_data$recall_df %>%
+    #   filter(recalling_firm %in% input$recalling_firm) %>%
+    #   filter(city %in% input$city) %>%
+    #   filter(state %in% input$state) %>%
+    #   #filter(country %in% input$country) %>%
+    #   #filter(distribution_pattern %in% input$distribution_pattern) %>%
+    #   filter(status %in% input$status)
   })
 
   # Function to clear search results when clear button is pressed
   observeEvent(input$clear_button, {
-    search_results$search_df <- NULL
+    recall_data$recall_df <- NULL
   })
 
   # Render the table of search results
-  output$recall_table <- renderDataTable({
-    if (!is.null(search_results$search_df)) {
-      search_results$search_df
-    } else {
-      recall_data$recall_df
-    }
-  })
+  output$recall_table <- renderTable({
+    recall_data$recall_df
+    })
 
   # output$recall_table <- renderTable({
   #
