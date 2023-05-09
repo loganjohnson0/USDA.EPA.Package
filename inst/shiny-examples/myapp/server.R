16d279e968ab38ddacd14e0dc492f4941d1116c8
Charchit Shukla <cshukla68@gmail.com>
2023-05-09 03:11:11 -0500

    Server and UI test for the package

--- inst/shiny-examples/myapp/server.R
+++ inst/shiny-examples/myapp/server.R
@@ -1,47 +1,10 @@
 #' @importFrom lubridate date, mdy_hm
-data <- read.csv("") %>%
-  mutate(Date.Reported = lubridate::date(mdy_hm(Date.Time.Reported)))
+
 
 server <- function(input, output) {
 
-  filtered_data <- reactive({
-    classification_filter <- evaluate_classification(input$)
-
-    data %>%
-
-      filter(between()) %>%
-
-      # classification filter
-      filter(Category %in% classification_filter)
+  output$result <- renderPrint({
+    sum_numbers(input$num1, input$num2)
   })
-
-  table_data <- reactive({
-    classification_filter <- evaluate_classification()
-
-    data %>%
-      filter(between()) %>%
-
-      # classification filter
-      filter(Category %in% classification_filter) %>%
-
-      select(state,
-             city)
-
-  })
-
-  output$map <- renderLeaflet({
-
-    leaflet(data) %>% addTiles() %>%
-      fitBounds(~min(longitude), ~min(latitude), ~max(longitude), ~max(latitude))
-  })
-
-  observe({
-    leaflet::leafletProxy("map", data = filtered_data()) %>%
-      leaflet::clearMarkers() %>%
-      leaflet::addMarkers(lng = ~longitude, lat = ~latitude,
-                 popup = ~as.character(Classification))
-  })
-
-  output$table <- renderDataTable(table_data())
 
 }
--- inst/shiny-examples/myapp/ui.R
+++ inst/shiny-examples/myapp/ui.R
@@ -8,30 +8,9 @@ library(tidyverse)
 # ui object
 ui <- fluidPage(
   titlePanel(p("FoodRecall", style = "color:#3474A7")),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId = "Classify",
-        label = "Select part of FDA",
-        ## please modify the selected option after updating the classification
-        choices = c("All",
-                    '',
-                    'Related'),
-        selected = "All"
-      ),
-      dateRangeInput(inputId =  'date',
-                     label = 'Years',
-                     start = "2012-01-01",
-                     end = "2012-01-02",
-                     min = "2012-01-01",
-                     format = "yyyy-mm-dd")
-    ),
-
-    mainPanel(
-      leafletOutput(outputId = "map"),
-      dataTableOutput((outputId = ""))
-    )
-  )
+  numericInput("num1", "Enter the first number:", value = 1),
+  numericInput("num2", "Enter the second number:", value = 2),
+  verbatimTextOutput("result")
 )
 
 
