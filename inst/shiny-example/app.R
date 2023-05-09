0fcbdb1ea9729efb91f2dc00e5c9c5e3a4588491
Logan Johnson <logan01@iastate.edu>
2023-05-09 15:18:21 -0500

    Combine server and ui

--- inst/shiny-example/app.R
+++ inst/shiny-example/app.R
@@ -1,3 +1,43 @@
+library(leaflet)
+library(shiny)
+library(DT)
+library(tidyverse)
+
+#' @importFrom leaflet leafletProxy, clearMarkers, addMarkers
+
+# ui object
+ui <- fluidPage(
+  titlePanel(p("FoodRecall", style = "color:#3474A7")),
+  sidebarLayout(
+    sidebarPanel(
+      textInput("api_key", "Enter your API key from FDI:"),
+      numericInput("limit", "Limit", value = 1000),
+      textInput("city", "City"),
+      #textInput("country", "Country"),
+      #textInput("distribution_pattern", "Distribution Pattern"),
+      textInput("recalling_firm", "Recalling Firm"),
+      #radioButtons("search_mode", "Search Mode", choices = c("AND", "OR"), selected = "AND"),
+      #textInput("state", "State"),
+      #textInput("status", "Status"),
+      selectInput("status", "Select Recall Status:",
+                  choices = c("", "Ongoing", "Terminated", "Completed", "Pending"),
+                  selected = ""),
+      br(),
+      actionButton("submit_button", "Submit"),
+      br(),
+      actionButton("clear_button", "Clear")
+
+    ),
+    mainPanel(
+      tableOutput("recall_table")
+    )
+  )
+)
+
+
+
+
+
 #' @importFrom lubridate date, mdy_hm
 
 
@@ -62,3 +102,5 @@ server <- function(input, output) {
   # })
 
 }
+
+shiny::shinyApp(ui,server)
